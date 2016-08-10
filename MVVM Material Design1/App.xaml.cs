f1a9a1fcfb52e46edf385cd055923b8fb8684f88
kanomkrub <tweerath.p@kanomkrub.com>
2016-08-11 05:12:12 +0700

    InitialCommitJa

---
+++ MVVM Material Design1/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MVVM_Material_Design1
+{
+    using System.Windows;
+    using ViewModels;
+    using Views;
+
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public void ApplicationStartup(object sender, StartupEventArgs e)
+        {
+            var mainWindowVm = new MainWindowVm();
+            var mainWindow = new MainWindow(mainWindowVm);
+
+            mainWindow.Show();
+        }
+    }
+}
---
+++ WpfApplication1/Views/BatchWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1.Views
+{
+    /// <summary>
+    /// Interaction logic for BatchWindow.xaml
+    /// </summary>
+    public partial class BatchWindow : UserControl
+    {
+        public BatchWindow()
+        {
+            InitializeComponent();
+        }
+        private void OnDialogClosing(object sender, DialogClosingEventArgs eventArgs)
+        {
+            Console.WriteLine("SAMPLE 1: Closing dialog with parameter: " + (eventArgs.Parameter ?? ""));
+
+            //you can cancel the dialog close:
+            //eventArgs.Cancel();
+
+            if (!Equals(eventArgs.Parameter, true)) return;
+
+            if (!string.IsNullOrWhiteSpace(SetNameDialog.Text))
+                testList.Items.Add(SetNameDialog.Text.Trim());
+        }
+    }
+}
