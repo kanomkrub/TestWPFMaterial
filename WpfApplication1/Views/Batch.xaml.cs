8ed930d410694b50ddb7c4cb7f645ed581591eac
004655 <tweerath.p@g-able.com>
2016-08-11 16:58:22 +0700

    Add Batch setting and doc list page

---
+++ WpfApplication1/Views/Batch.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1.Views
+{
+    /// <summary>
+    /// Interaction logic for Batch.xaml
+    /// </summary>
+    public partial class Batch : UserControl
+    {
+        BatchSetting batchSettingControl = new BatchSetting();
+        DocumentList docListControl = new DocumentList();
+        public Batch()
+        {
+            InitializeComponent();
+            contentControl.Content = docListControl;
+            batchLabel.Text = "Document List..";
+        }
+        public void MenuPopupButton_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void config_Checked(object sender, RoutedEventArgs e)
+        {
+            contentControl.Content = batchSettingControl;
+            batchLabel.Text = "Settings";
+        }
+
+        private void config_UnChecked(object sender, RoutedEventArgs e)
+        {
+            contentControl.Content = docListControl;
+            batchLabel.Text = "Document List..";
+        }
+    }
+}
---
+++ WpfApplication1/Views/DocumentList.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1.Views
+{
+    /// <summary>
+    /// Interaction logic for DocumentList.xaml
+    /// </summary>
+    public partial class DocumentList : UserControl
+    {
+        public DocumentList()
+        {
+            InitializeComponent();
+            docGrid.ItemsSource = Document.LoadCollectionData();
+        }
+
+    }
+    public class Document
+    {
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime ExportDate { get; set; }
+
+        public string Status { get; set; }
+        
+        public static List<Document> LoadCollectionData()
+        {
+            List<Document> doclist = new List<Document>();
+            doclist.Add(new Document()
+            {
+                ID = 101,
+                Name = "document 1",
+                Status = "exported",
+                ExportDate = new DateTime(1975, 2, 23),
+            });
+
+            doclist.Add(new Document()
+            {
+                ID = 201,
+                Name = "document 2",
+                Status = "exported",
+                ExportDate = new DateTime(1982, 4, 12),
+            });
+
+            doclist.Add(new Document()
+            {
+                ID = 244,
+                Name = "document 3",
+                Status = "exported",
+                ExportDate = new DateTime(1985, 9, 11),
+            });
+            return doclist;
+        }
+    }
+}
